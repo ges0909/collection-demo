8dff7120ad646f754747bad7d74b735e4b43cea6
ges0909 <gerrit.schrader@gmail.com>
2019-04-25 06:46:03 +0200

    set operations added

--- src/test/java/schrader/collection/test/CollectionTest.java
+++ src/test/java/schrader/collection/test/CollectionTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CollectionTest {
 
@@ -15,12 +17,12 @@ class CollectionTest {
     class Java9AddOns {
 
         @Test
-        void listOf() {
+        void immutableListOf() {
             var l = List.of(1, 2, 3);
         }
 
         @Test
-        void setOf() {
+        void immutableSetOf() {
             var s = Set.of(1, 2, 3);
         }
 
@@ -40,6 +42,64 @@ class CollectionTest {
                     Map.entry("2", "two"),
                     Map.entry("3", "three")
             );
+        }
+    }
+
+    @Nested
+    @DisplayName("Durchschnitt, Vereinigung und Differenz")
+    class SetOperations {
+
+        @Test
+        void intersection() {
+            var s = new HashSet<Integer>(Arrays.asList(1, 2, 3)); // mutable
+            var s2 = Set.of(2, 3, 4); // immutable
+            s.retainAll(s2);
+            assertThat(s).containsExactly(2, 3);
+        }
+
+        @Test
+        void union() {
+            var s = new HashSet<Integer>(Arrays.asList(1, 2, 3)); // mutable
+            var s2 = Set.of(2, 3, 4); // immutable
+            s.addAll(s2);
+            assertThat(s).containsExactly(1, 2, 3, 4);
+        }
+
+        @Test
+        void difference() {
+            var s = new HashSet<Integer>(Arrays.asList(1, 2, 3)); // mutable
+            var s2 = Set.of(2, 3, 4); // immutable
+            s.removeAll(s2);
+            assertThat(s).containsExactly(1);
+        }
+
+        @Test
+        void intersectionWithStream() {
+            var s1 = Set.of(1, 2, 3);
+            var s2 = Set.of(2, 3, 4);
+            var s = s1.stream()
+                    .filter(s2::contains)
+                    .collect(Collectors.toSet());
+            assertThat(s).containsExactly(2, 3);
+        }
+
+        @Test
+        void unionWithStream() {
+            var s1 = Set.of(1, 2, 3);
+            var s2 = Set.of(2, 3, 4);
+            var s = Stream.concat(s1.stream(), s2.stream())
+                    .collect(Collectors.toSet());
+            assertThat(s).containsExactly(1, 2, 3, 4);
+        }
+
+        @Test
+        void differenceWithStream() {
+            var s1 = Set.of(1, 2, 3);
+            var s2 = Set.of(2, 3, 4);
+            var s = s1.stream()
+                    .filter(val -> !s2.contains(val))
+                    .collect(Collectors.toSet());
+            assertThat(s).containsExactly(1);
         }
     }
 }
